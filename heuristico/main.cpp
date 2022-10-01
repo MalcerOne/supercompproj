3759edd9f1649f8943f930f011a395b99bf490bf
MalcerOne <r.malcervelli@gmail.com>
2022-10-01 16:10:45 -0300

    heuristico pronto

--- heuristico/main.cpp
+++ heuristico/main.cpp
@@ -8,7 +8,7 @@
 #include <cmath>
 using namespace std;
 
-// ++ Structs ++
+// ======================== Struct ========================
 // Estrutura da cidade
 struct City {
     int idx;
@@ -16,22 +16,22 @@ struct City {
     double y;
 };
 
-// ++ Funcoes ++
+// ======================== Funcoes ========================
 // Funcao para calcular a distancia entre duas cidades
-double calcDistance(City a, City b) {
+double calc_distance(City a, City b) {
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
 // Funcao para calcular cidade proxima
-City closestCity(vector<City> cities, City currentCity) {
+City closest_city(vector<City> cities, City currentCity) {
     double minDistance = 10000000000;
     double dist;
     City closerCity;
 
-    for (int i = 0; i < cities.size(); i++) {
+    for (int i = 0; i < (int) cities.size(); i++) {
         if (cities[i].idx != currentCity.idx) {
             // Se a cidade iterada for diferente da cidade atual, calcula a distancia.
-            dist = calcDistance(currentCity, cities[i]);
+            dist = calc_distance(currentCity, cities[i]);
 
             if (dist < minDistance) {
                 minDistance = dist;
@@ -41,7 +41,6 @@ City closestCity(vector<City> cities, City currentCity) {
     }
     return closerCity;
 }
-
 
 // Funcao main
 int main() {
@@ -64,32 +63,47 @@ int main() {
     City currentCity = cities[0];
     vector<City> path;
     double totalDistance = 0;
+    int count = 0;
 
-    path.push_back(currentCity);
-
+    path.push_back(currentCity); // Adiciona a primeira cidade ao caminho
+        
     // Enquanto o caminho nao tiver todas as cidades
-    while (path.size() < n) {
+    while ((int) path.size() < n) {
+        count ++;
+
+        // Remove primeira cidade da iteracao. 
+        if (count == 1){
+            cities.erase(cities.begin());
+        }
+
         // Encontra a cidade mais proxima
-        City closerCity = closestCity(cities, currentCity);
+        City closerCity = closest_city(cities, currentCity);
+        
+        // Remove a cidade mais proxima do vetor de cidades
+        for (int i = 0; i < (int) cities.size(); i++) {
+            if (cities[i].idx == closerCity.idx) {
+                cities.erase(cities.begin() + i);
+            }
+        }
 
         // Adiciona a cidade mais proxima ao caminho
         path.push_back(closerCity);
 
         // Calcula a distancia entre a cidade atual e a cidade mais proxima
-        totalDistance += calcDistance(currentCity, closerCity);
+        totalDistance += calc_distance(currentCity, closerCity);
 
         // Atualiza a cidade atual
         currentCity = closerCity;
     }
 
     // Calcula a distancia entre a ultima cidade e a primeira
-    totalDistance += calcDistance(currentCity, path[0]);
+    totalDistance += calc_distance(currentCity, path[0]);
 
     // Imprime a distancia total
     cout << totalDistance << " " << "0" << endl;
 
     // Imprime o caminho e a distancia total
-    for (int i = 0; i < path.size(); i++) {
+    for (int i = 0; i < (int) path.size(); i++) {
         cout << path[i].idx << " ";
     }
     cout << endl;
