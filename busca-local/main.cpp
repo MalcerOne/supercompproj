afe5cdc3a144a90f272ffc48dd58ca95e17cf39e
MalcerOne <r.malcervelli@gmail.com>
2022-10-03 13:17:13 -0300

    trying busca-local

---
+++ busca-local/main.cpp
@@ -0,0 +1,107 @@
+// Metodologia de busca local para solucao do problema do caixeiro viajante
+// Autor: Rafael Malcervelli
+// Email: rafaelsm9@al.insper.edu.br
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+// ======================== Struct ========================
+// Estrutura da cidade
+struct City {
+    int idx;
+    double x;
+    double y;
+};
+
+// ======================== Funcoes ========================
+// Funcao para calcular a distancia entre duas cidades
+double calc_distance(City a, City b) {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+
+// Funcao para calcular a distancia total percorrida
+double calc_total_distance(vector<City> cities) {
+    double total_distance = 0;
+
+    for (int i = 0; i < (int) cities.size() - 1; i++) {
+        total_distance += calc_distance(cities[i], cities[i + 1]);
+    }
+
+    total_distance += calc_distance(cities[cities.size() - 1], cities[0]);
+
+    return total_distance;
+}
+
+int main() {
+    // Quantidade de cidades
+    int n;
+    cin >> n;
+
+    // Atribuicao das cidades e suas coordenadas
+    vector<City> cities;
+    double total_distance = 0;
+
+    for (int i = 0; i < n; i++) {
+        City city;
+        cin >> city.x;
+        cin >> city.y;
+        city.idx = i;
+
+        cities.push_back(city);
+    }
+
+    // Geracao de numero aleatorio
+    srand(unsigned(time(0)));
+
+    // ======================== Busca Local ========================
+    for (int i = 0; i < (10 * n); i++) {
+        // Geracao de numero aleatorio
+        random_shuffle(cities.begin(), cities.end());
+
+        // Calculo da distancia total
+        total_distance = calc_total_distance(cities);
+
+        for (int main_vector_idx = 0; main_vector_idx < (int) cities.size(); main_vector_idx++) {
+            for (int aux_vector_idx = main_vector_idx + 1; aux_vector_idx < (int) cities.size(); aux_vector_idx++) {
+                if (main_vector_idx != aux_vector_idx) {
+                    // Troca de posicao das cidades
+                    vector<City> cities_aux = cities;
+                    swap(cities_aux[main_vector_idx], cities_aux[aux_vector_idx]);
+
+                    // Calculo da distancia total com base na troca realizada
+                    double total_distance_aux = calc_total_distance(cities_aux);
+
+                    // Verificacao se a distancia total e menor
+                    if (total_distance_aux < total_distance) {
+                        cities = cities_aux;
+                        total_distance = total_distance_aux;
+                    } else {
+                        swap(cities_aux[main_vector_idx], cities_aux[aux_vector_idx]);
+                    }
+                }
+                
+            }
+        }
+        // Error
+        cerr << "local: " << total_distance << "";
+        
+        // Imprime a sequencia de cidades
+        for (int i = 0; i < (int) cities.size(); i++) {
+            cerr << cities[i].idx << " ";
+        }
+        cerr << endl;
+    }
+
+    // ======================== Resultado ========================
+    cout << total_distance << " 0" << endl;
+    for (int i = 0; i < (int) cities.size(); i++) {
+        cout << cities[i].idx << " ";
+    }
+    cout << endl;
+    
+}
